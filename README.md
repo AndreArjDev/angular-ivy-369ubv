4e9872d4712f28455a4535de9453f35006912e3b
AndreArjDev <andre.021arj@gmail.com>
2021-11-03 12:03:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-369ubv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-369ubv)
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { CepListComponent } from './cep-list/cep-list.component';
+import { HomeComponent } from './home/home.component';
+import { CepComponent } from './cep/cep.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'list', component: CepListComponent },
+      { path: 'ceps/:numeroCep', component: CepComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    CepListComponent,
+    HomeComponent,
+    CepComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
